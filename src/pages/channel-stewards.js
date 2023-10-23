d763bd61a10688ae74498cf8a56cdf0abb54e7c8
Jen Reiher <jen@jens-mbp.lan>
2023-10-22 18:10:04 -0700

    Fix button and add training requirement

--- src/pages/channel-stewards.js
+++ src/pages/channel-stewards.js
@@ -22,6 +22,7 @@ const ChannelStewards = ({ data }) => {
         <h2 className="mt-12">Channel Stewardship Responsibilities</h2>
         <p>Channel Stewards are responsible both for stewarding their channels, and supporting the Steward community to grow and mature. We strive for their role to look like this:</p>
         <ul>
+          <li>Participate in <a href="https://www.tidalequality.com/equity-sequence">Equity Sequence</a> training.</li>
           <li>Monitor their channel(s) and the mod forum for potential issues regularly.</li>
           <li>Support others to work through active issues collectively as they arise.</li>
           <li>Help cover for others if they have to step away for some reason.</li>
@@ -35,11 +36,12 @@ const ChannelStewards = ({ data }) => {
 
         <div className="card-grid grid grid-cols-3 gap-4">
           {stewards.map((steward, idx) => <ChannelStewardCard steward={stewards[idx]} key={idx}/>)}
-          <Button
+          <a
             href="https://docs.google.com/forms/d/e/1FAIpQLSfWJE2PAN8_20YN0xd3kymzBuRsmkWLjwLlnoVasud1-olyUw/viewform?usp=sf_link"
+            className="text-center rounded-xl py-4 bg-secondary px-5 font-heading w-full text-white font-semibold uppercase tracking-widest mt-5 transition-colors duration-200 no-underline drop-shadow-sm hover:drop-shadow-none"
           >
             Apply to be a Channel Steward
-          </Button>
+          </a>
         </div>
       </main>
     </Layout>
